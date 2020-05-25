8b69af15b13d188867f53a351d79579ceed6ced2
Ushiosan23 <haloleyendee@outlook.com>
2020-05-24 23:25:59 -0500

    feat(core): Create Comment class
    This class is used to save comment configurations

---
+++ src/fx/codecounter/data/format/Comment.kt
@@ -0,0 +1,26 @@
+package fx.codecounter.data.format
+
+/**
+ * Comment data identify
+ *
+ * @param shortComment a line of comment
+ * @param longCommentStart start long comment (multiline comment)
+ * @param longCommentEnd end long comment (multiline comment)
+ */
+data class Comment(
+	
+	/**
+	 * Short comment regex
+	 */
+	val shortComment: String,
+	
+	/**
+	 * Long start comment regex
+	 */
+	val longCommentStart: String,
+	
+	/**
+	 * Long end comment regex
+	 */
+	val longCommentEnd: String
+)
