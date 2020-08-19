fc83a758977afc721bbb086bdc1c8d2c66a929f4
Ushio Okasaki <haloleyendee@outlook.com>
2020-08-19 16:37:43 -0500

    refactor(kotlinCore): Remove unused classes
    These classes were removed because they will be created under another name and in another package.

--- src/main/kotlin/fx/soft/codecounter/reader/FileType.kt
+++
@@ -1,9 +0,0 @@
-package fx.soft.codecounter.reader
-
-data class FileType(
-	val fileName: String,
-	val extensions: List<String>,
-	val singleComment: Regex,
-	val startMultipleComment: Regex,
-	val endMultipleComment: Regex
-)
